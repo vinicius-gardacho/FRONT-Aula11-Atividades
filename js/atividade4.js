a71d1ee4baa482887a8d15b7a2765e0f885a9b21
vinicius <engs-viniciusgardacho@camporeal.edu.br>
2026-04-27 19:10:11 -0300

    upload arquivos mandados pelo prof

---
+++ js/atividade4.js
@@ -0,0 +1,17 @@
+// ATIVIDADE 4 (base da atividade 3)
+// ==============================
+
+// Dentro do evento de adicionar item:
+
+// 1. Criar o botão de remover
+// → usar document.createElement()
+
+// 2. Adicionar texto ao botão
+// → exemplo: "Remover" ou "X"
+
+// 3. Adicionar evento de clique no botão
+// → quando clicar:
+//    - remover o item da lista (li.remove())
+
+// 4. Colocar o botão dentro do <li>
+// → usar appendChild()
---
+++ js/atividade5.js
@@ -0,0 +1,31 @@
+// ATIVIDADE 5 (base da atividade 3)
+// ==============================
+
+// PARTE 1 — Criar função para adicionar tarefas
+// → função responsável por criar o <li>
+// → reutilizar código da atividade 3
+
+// PARTE 2 — Evento do botão "Adicionar"
+// → usar addEventListener
+// → pegar valor do input
+// → chamar função que cria o <li>
+// → limpar o input
+
+// PARTE 3 — Salvar dados no localStorage
+// → criar função salvar()
+// → percorrer todos os <li>
+// → pegar o texto de cada item
+// → guardar em uma lista (array)
+// → transformar em JSON (JSON.stringify)
+// → salvar no localStorage
+
+// PARTE 4 — Carregar dados ao abrir a página
+// → usar: document.addEventListener("DOMContentLoaded")
+// → buscar dados do localStorage
+// → transformar JSON em lista (JSON.parse)
+// → recriar os <li> na tela
+
+// PARTE 5 — Atualizar sempre que mudar
+// → chamar salvar() ao:
+//    - adicionar item
+//    - Carregar a página
