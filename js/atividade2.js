47cb9b858dcd7cfcd82ad523b3fd130e1d62df42
vinicius <engs-viniciusgardacho@camporeal.edu.br>
2026-04-27 19:57:51 -0300

    atualização para if ternario atividade 2

--- js/atividade2.js
+++ js/atividade2.js
@@ -3,18 +3,9 @@ const botaoTexto = document.getElementById("botao-texto")
 const botaoFundo = document.getElementById("botao-fundo")
 
 botaoTexto.addEventListener("click", function(){
-    
-    if(paragrafo.style.color == "black"){
-        paragrafo.style.color = "red"
-    } else{
-        paragrafo.style.color = "black"
-    }
+    paragrafo.style.color = paragrafo.style.color == "black" ? paragrafo.style.color = "red" : paragrafo.style.color = "black"
 })
 
 botaoFundo.addEventListener("click", function(){
-    if (document.body.style.backgroundColor == "white") {
-        document.body.style.backgroundColor = "blue"
-    } else{
-        document.body.style.backgroundColor = "white"
-    }
+    document.body.style.backgroundColor = document.body.style.backgroundColor == "white" ? document.body.style.backgroundColor = "blue" :  document.body.style.backgroundColor = "white"
 })
